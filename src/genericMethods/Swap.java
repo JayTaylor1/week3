dd8bdbe0ceae2f3d1c8d2920dc4863e631e43711
JayTaylor1 <jbt2000@live.co.uk>
2019-12-16 17:45:19 +0000

    Updated With Documentation

--- src/genericMethods/Swap.java
+++ src/genericMethods/Swap.java
@@ -1,29 +1,40 @@
 package genericMethods;
-
 import java.util.Arrays;
-
 /**
  * A demonstration of a generic method.
  *
- * This class defines a (rather superfluous) static method that will test for
- * equality (according to the equals() method!) of two objects of the same type.
+ * This class defines a static method that will swap two objects of the same type at the given indices.
  *
- * @author Hugh Osborne
- * @version October 2019
+ * @author Jay Taylor
+ * @version 2019
  */
 public class Swap {
-
     /**
-     * Check if two objects are equal
+     * Swap two objects of the same type at the given indices
      * @param <T> the type of objects to be compared
-     * @param object1 the first object
-     * @param object2 the second object
-     * @return true if the objects are equal (according to the equals() method)
+     * @param index1 the first index to swap
+     * @param index2 the index to swap with the first one
+     * @param array the initial unswapped array
+     * @return the updated array with the swapped indices
+     * @throws IndexingError if one of the indices are out of bounds
+     * <ul>
+     *     <li> Set a temporary ocject equal to the first index</li>
+     *     <li> Set the first index equal to the second index</li>
+     *     <li> set the second index equal to the temp object</li>
+     *     <li> Return the new array</li>
+     * </ul>
      */
-    public static <T> T[] swap(T[] array, int index1, int index2) {
+    public static <T> T[] swap(T[] array, int index1, int index2) throws IndexingError {
+        //Check to see if the two indices are valid
+        if (index1 < 0 || index1 > array.length || index2 < 0 || index2 > array.length) {
+            throw new IndexingError();
+        }
+        //Set a temp object to hold the first index
         T temp = array[index1];
+        //set the first index to be equal to the second index
         array[index1] = array[index2];
+        //set the second index to be equal to the temp object
         array[index2] = temp;
-        return array;
+        return array;   //Return the swapped array
     }
 }
--- tests/genericMethods/SwapTest.java
+++ tests/genericMethods/SwapTest.java
@@ -1,37 +1,31 @@
 package genericMethods;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 class SwapTest {
-
     /**
-     * A selection of tests for equals
+     * A selection of tests for checking whether the Swap call returns the
+     * right array when executed
+     * @throws IndexingError if one of the indexs are out of bounds
      */
 	 String[] array = {"Tony","Andrew","Hugh","Diane","Simon","Gary"};
-
     @Test
-    void testSwapIntegers() {
+    void testSwapIntegers() throws IndexingError {
         Integer[] array = {5,2,9,-7,3,2,12,0};
         Integer[] expected = {-7,2,9,5,3,2,12,0};
         assertArrayEquals(expected,Swap.swap(array,0, 3));
     }
-
     @Test
-    void testSwapIntegers2() {
+    void testSwapIntegers2() throws IndexingError {
         Integer[] array = {5,2,9,-7,3,2,12,0};
         Integer[] expected = {0,2,9,-7,3,2,12,5};
         assertArrayEquals(expected,Swap.swap(array,0, 7));
     }
     @Test
-    void testSwapStrings() {
+    void testSwapStrings() throws IndexingError {
         String[] array = {"Tony","Andrew","Hugh","Diane","Simon","Gary"};
         String[] expected = {"Tony","Simon","Hugh","Diane","Andrew","Gary"};
-        assertArrayEquals(expected,Swap.swap(array,1, 4));
+        assertArrayEquals(expected,Swap.swap(array,1, 4)) ;
     }
-
-
-
 }
